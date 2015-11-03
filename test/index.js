f567b67e873c2dd7175aa26e6be2eee9267a47d2
Shinichi Kogai <kogai0121@gmail.com>
2015-11-03 21:11:47 +0900

    Change form-parts items as Object to Array
    Probably user want to fix order of items.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import assert from 'power-assert';
 import VueForm from '../lib';
 
-describe('mochaのテスト', ()=> {
+describe('VueFormインスタンスのテスト', ()=> {
   it('インスタンスに名前を渡せる', ()=> {
     const myForm = new VueForm({
       name: 'my-form',
@@ -11,93 +11,96 @@ describe('mochaのテスト', ()=> {
 
   it('デフォルトの名前を持つ', ()=> {
     const myForm = new VueForm();
-    assert(myForm.name === 'vue-form-unnamed');
+    assert(myForm.name === 'vue-form');
   });
 
   it('デフォルトのフォームアイテムを持つ', ()=> {
     const myForm = new VueForm();
-    assert(myForm.items.text.type === 'text');
-    assert(myForm.items.text.className === 'form-item__text');
-    assert(myForm.items.text.dirty === true);
-    assert(myForm.items.text.required === true);
-    assert(myForm.items.text.minLength === 5);
-    assert(myForm.items.text.maxLength === 15);
+    assert(myForm.items[0].type === 'text');
+    assert(myForm.items[0].className === 'form-item__text');
+    assert(myForm.items[0].dirty === true);
+    assert(myForm.items[0].required === true);
+    assert(myForm.items[0].minLength === 5);
+    assert(myForm.items[0].maxLength === 15);
 
-    assert(myForm.items.email.type === 'email');
-    assert(myForm.items.email.className === 'form-item__email');
-    assert(myForm.items.email.dirty === true);
-    assert(myForm.items.email.required === true);
-    assert(myForm.items.email.isEmail === true);
+    assert(myForm.items[1].type === 'email');
+    assert(myForm.items[1].className === 'form-item__email');
+    assert(myForm.items[1].dirty === true);
+    assert(myForm.items[1].required === true);
+    assert(myForm.items[1].isEmail === true);
 
-    assert(myForm.items.password.type === 'password');
-    assert(myForm.items.password.className === 'form-item__password');
-    assert(myForm.items.password.dirty === true);
-    assert(myForm.items.password.required === true);
-    assert(myForm.items.password.minLength === 5);
-    assert(myForm.items.password.maxLength === 30);
+    assert(myForm.items[2].type === 'password');
+    assert(myForm.items[2].className === 'form-item__password');
+    assert(myForm.items[2].dirty === true);
+    assert(myForm.items[2].required === true);
+    assert(myForm.items[2].minLength === 5);
+    assert(myForm.items[2].maxLength === 30);
   });
 
   it('フォームアイテムを設定できる', ()=> {
     const myForm = new VueForm({
-      items: {
-        username: {
+      items: [
+        {
+          name: 'username',
           type: 'text',
           required: true,
           minLength: 3,
           maxLength: 10,
         },
-      },
+      ],
     });
-    assert(myForm.items.username.type === 'text');
-    assert(myForm.items.username.className === 'form-item__username');
-    assert(myForm.items.username.dirty === false);
-    assert(myForm.items.username.required === true);
-    assert(myForm.items.username.minLength === 3);
-    assert(myForm.items.username.maxLength === 10);
+    assert(myForm.items[0].type === 'text');
+    assert(myForm.items[0].className === 'form-item__username');
+    assert(myForm.items[0].dirty === false);
+    assert(myForm.items[0].required === true);
+    assert(myForm.items[0].minLength === 3);
+    assert(myForm.items[0].maxLength === 10);
   });
 
-  it.only('デフォルトのフォームアイテムは可能な限り維持される', ()=> {
+  it('デフォルトのフォームアイテムは可能な限り維持される', ()=> {
     const myForm = new VueForm({
-      items: {
-        username: {
+      items: [
+        {
+          name: 'username',
           type: 'text',
           required: true,
           minLength: 3,
           maxLength: 10,
         },
-      },
+      ],
     });
-    assert(myForm.items.username.type === 'text');
-    assert(myForm.items.username.className === 'form-item__username');
-    assert(myForm.items.username.dirty === false);
-    assert(myForm.items.username.required === true);
-    assert(myForm.items.username.minLength === 3);
-    assert(myForm.items.username.maxLength === 10);
+    assert(myForm.items[0].type === 'text');
+    assert(myForm.items[0].className === 'form-item__username');
+    assert(myForm.items[0].dirty === false);
+    assert(myForm.items[0].required === true);
+    assert(myForm.items[0].minLength === 3);
+    assert(myForm.items[0].maxLength === 10);
 
-    assert(myForm.items.text.type === 'text');
-    assert(myForm.items.text.className === 'form-item__text');
-    assert(myForm.items.text.dirty === true);
-    assert(myForm.items.text.required === true);
-    assert(myForm.items.text.minLength === 5);
-    assert(myForm.items.text.maxLength === 15);
+    assert(myForm.items[1].type === 'text');
+    assert(myForm.items[1].className === 'form-item__text');
+    assert(myForm.items[1].dirty === true);
+    assert(myForm.items[1].required === true);
+    assert(myForm.items[1].minLength === 5);
+    assert(myForm.items[1].maxLength === 15);
   });
 
-  it.only('フォームアイテムを上書きできる', ()=> {
+  it('フォームアイテムを上書きできる', ()=> {
     const myForm = new VueForm({
-      items: {
-        password: {
+      items: [
+        {
+          name: 'password',
           type: 'password',
           className: 'form-item__password--confirm',
           required: false,
           minLength: 1,
           maxLength: 100,
         },
-      },
+      ],
     });
-    assert(myForm.items.password.type === 'password');
-    assert(myForm.items.password.className === 'form-item__password--confirm');
-    assert(myForm.items.password.required === false);
-    assert(myForm.items.password.minLength === 1);
-    assert(myForm.items.password.maxLength === 100);
+    assert(myForm.items[0].type === 'password');
+    assert(myForm.items[0].className === 'form-item__password--confirm');
+    assert(myForm.items[0].required === false);
+    assert(myForm.items[0].minLength === 1);
+    assert(myForm.items[0].maxLength === 100);
   });
 });
