62ca3b3d7475ed2f4ca24613cf04b12d4a86e72f
Shinichi Kogai <kogai0121@gmail.com>
2015-11-03 23:29:32 +0900

    Add browser environment in Node.js test environment.

--- test/install.js
+++ test/install.js
@@ -1,15 +1,61 @@
 import assert from 'power-assert';
 import Vue from 'vue';
+import jsdom from 'jsdom';
 import VueForm from '../lib';
 
-describe('Vue.installのテスト', ()=> {
+describe('Vue.install', ()=> {
   const myForm = new VueForm();
+  let vm;
   before((done)=> {
-    Vue.use(myForm);
-    Vue.nextTick(done);
+    jsdom.env({
+      html: `
+        <html><body>
+          <div id="root">
+            {{test}}
+            <vue-form></vue-form>
+          </div>
+        </body></html>
+      `,
+      done(err, window) {
+        assert(err === null);
+        global.document = window.document;
+        global.window = window.document.defaultView;
+        global.navigator = window.navigator;
+        global.DocumentFragment = window.DocumentFragment;
+        vm = new Vue({
+          el: '#root',
+          data: {
+            test: 'This is test.',
+          },
+        });
+        Vue.use(myForm);
+        Vue.nextTick(done);
+      },
+    });
   });
 
-  it('Vueにインストールできる', ()=> {
-    assert(myForm.name === 'vue-form');
+  it('Can Install Plugin to Vue.', ()=> {
+    assert(typeof Vue.options.elementDirectives['vue-form'] === 'object');
+    assert(typeof Vue.options.elementDirectives['vue-form'].bind === 'function');
+  });
+
+  it('Can create DOM', ()=> {
+    assert(vm.$el.innerHTML.match('This is test.'));
+  });
+
+  it.only('Can create custom element-directives.', ()=> {
+    assert(vm.$el.innerHTML.match('<div class="vue-form-item">'));
+
+    assert(vm.$el.innerHTML.match('<dl class="vue-form-item__username">'));
+    assert(vm.$el.innerHTML.match('<dt>username</dt>'));
+    assert(vm.$el.innerHTML.match('<dd><input type="text" v-model="formUsername" /></dd>'));
+
+    assert(vm.$el.innerHTML.match('<dl class="vue-form-item__email">'));
+    assert(vm.$el.innerHTML.match('<dt>email</dt>'));
+    assert(vm.$el.innerHTML.match('<dd><input type="text" v-model="formEmail" /></dd>'));
+
+    assert(vm.$el.innerHTML.match('<dl class="vue-form-item__password">'));
+    assert(vm.$el.innerHTML.match('<dt>password</dt>'));
+    assert(vm.$el.innerHTML.match('<dd><input type="password" v-model="formPassword" /></dd>'));
   });
 });
