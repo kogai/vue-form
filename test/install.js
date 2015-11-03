dbb55bb39a494cb88beaf494f47375c2ab4346d2
Shinichi Kogai <kogai0121@gmail.com>
2015-11-04 08:06:32 +0900

    Add default test-case of custom element-directives.

--- test/install.js
+++ test/install.js
@@ -4,7 +4,13 @@ import jsdom from 'jsdom';
 import VueForm from '../lib';
 
 describe('Vue.install', ()=> {
-  const myForm = new VueForm();
+  const myForm = new VueForm({
+    items: [
+      {
+        name: 'username',
+      },
+    ],
+  });
   let vm;
   before((done)=> {
     jsdom.env({
@@ -43,19 +49,20 @@ describe('Vue.install', ()=> {
     assert(vm.$el.innerHTML.match('This is test.'));
   });
 
-  it.only('Can create custom element-directives.', ()=> {
+  it('Can create custom element-directives.', ()=> {
     assert(vm.$el.innerHTML.match('<div class="vue-form-item">'));
 
     assert(vm.$el.innerHTML.match('<dl class="vue-form-item__username">'));
     assert(vm.$el.innerHTML.match('<dt>username</dt>'));
-    assert(vm.$el.innerHTML.match('<dd><input type="text" v-model="formUsername" /></dd>'));
+    assert(vm.$el.innerHTML.match('<input type="text" v-model="formUsername">'));
+    assert(vm.$el.innerHTML.match('<dd><input type="text" v-model="formUsername"></dd>'));
 
     assert(vm.$el.innerHTML.match('<dl class="vue-form-item__email">'));
     assert(vm.$el.innerHTML.match('<dt>email</dt>'));
-    assert(vm.$el.innerHTML.match('<dd><input type="text" v-model="formEmail" /></dd>'));
+    assert(vm.$el.innerHTML.match('<dd><input type="email" v-model="formEmail"></dd>'));
 
     assert(vm.$el.innerHTML.match('<dl class="vue-form-item__password">'));
     assert(vm.$el.innerHTML.match('<dt>password</dt>'));
-    assert(vm.$el.innerHTML.match('<dd><input type="password" v-model="formPassword" /></dd>'));
+    assert(vm.$el.innerHTML.match('<dd><input type="password" v-model="formPassword"></dd>'));
   });
 });
