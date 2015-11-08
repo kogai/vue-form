ce7c9231638dea9c07d50b6752660aaab959b82c
Shinichi Kogai <kogai0121@gmail.com>
2015-11-09 07:22:10 +0900

    コンテキストを共有していた

--- example/app.js
+++ example/app.js
@@ -2,12 +2,30 @@ import Vue from 'vue';
 import VueForm from '../lib';
 
 const defaultForm = new VueForm();
+const customForm = new VueForm({
+  name: 'your-custom-name',
+  items: [
+    {
+      type: 'text',
+      name: '項目の名前',
+      dirty: true,
+      errorMessage: '不正規な値です',
+      minLength: 3,
+      maxLength: 5,
+      required: true,
+    },
+  ],
+});
 Vue.use(defaultForm);
+Vue.use(customForm);
 
 new Vue({
   el: '#root',
   created() {
     defaultForm.on((inputed)=> {
+      console.log(inputed);
+    });
+    customForm.on((inputed)=> {
       console.log(inputed);
     });
   },
